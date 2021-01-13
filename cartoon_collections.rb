266eb10e4e031dce37b5748cbc688c0ce37cf752
Reid <rlj0713@gmail.com>
2021-01-13 15:48:06 -0500

    commit tree cb63059d953bc805ff77b657f48232f8b347ca78

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|element, index| puts "#{index + 1} #{element}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  new_array = []
+  planeteer_calls.each {|call| new_array << call.capitalize + "!"}
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.detect{|element| element.length > 4}
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?("camembert")
+    return "camembert"
+  elsif array.include?("cheddar")
+    return "cheddar"
+  elsif array.include?("gouda")
+    return "gouda"
+  else
+    nil
+  end
 end
